85270179de481ccebe4c3cb849b5a2c71315be6f
EC2 Default User <ec2-user@ip-172-31-78-192.ec2.internal>
2018-07-30 03:07:06 +0000

    add bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,21 +4,28 @@
 
     <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        {{Form::select('status', [
-           '開始' => '開始',
-           '終了' => '終了',
-           '作業中' => '作業中']
-        )}}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {{Form::select('status', [
+                       '開始' => '開始',
+                       '終了' => '終了',
+                       '作業中' => '作業中']
+                    )}}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            
+                    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                </div>
+            
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
